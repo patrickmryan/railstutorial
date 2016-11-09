95ea543a39686303fe905161dd358bf99da270ad
Patrick M. Ryan <pmryan@us.ibm.com>
2016-11-09 14:59:59 -0500

    more stuff, up to p. 109

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   self.get('static_pages/home')
-
   self.get('static_pages/help')
-
+  self.get('static_pages/about')
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
