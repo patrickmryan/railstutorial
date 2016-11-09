ad72064331e59411f73a2a099ef059d0d33982e6
Patrick M. Ryan <pmryan@us.ibm.com>
2016-11-09 15:40:03 -0500

    more refactoring

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -3,17 +3,20 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     self.get(static_pages_home_url)
-    assert_response :success
+    self.assert_response(:success)
+    self.assert_select("title", "Home | Ruby on Rails Tutorial Sample App")
   end
 
   test "should get help" do
     self.get(static_pages_help_url)
     assert_response :success
+    self.assert_select("title", "Help | Ruby on Rails Tutorial Sample App")
   end
 
   test "should get about" do
     self.get(static_pages_about_url)
     assert_response :success
+    self.assert_select("title", "About | Ruby on Rails Tutorial Sample App")
   end
   
 end
