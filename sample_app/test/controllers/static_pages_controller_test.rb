d0cb9fdb82b66182be8818e7faa4166279ffe19e
Patrick M. Ryan <pmryan@us.ibm.com>
2016-11-10 08:59:33 -0500

    more testing

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,41 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-#  test "should get home" do
-#    self.get(static_pages_home_url)
-#    self.assert_response(:success)
-#    self.assert_select("title", "Home | Ruby on Rails Tutorial Sample App")
-#  end
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "should get home" do
+    print "root = " + root_url +  "\n"
+    self.get(root_url)
+    self.assert_response(:success)
+    self.assert_select("title", "Home | #{@base_title}")
+  end
 
   test "should get help" do
-    self.get(static_pages_help_url)
+    url = static_pages_help_url
+    print "help url = " + url +  "\n"
+    self.get(url)
     assert_response :success
-    self.assert_select("title", "Help | Ruby on Rails Tutorial Sample App")
+    self.assert_select("title", "Help | #{@base_title}")
   end
 
   test "should get about" do
-    self.get(static_pages_about_url)
+    url = static_pages_about_url
+    print "about url = " + url +  "\n"
+    self.get(url)    
+    #self.get(static_pages_about_url)
     assert_response :success
-    self.assert_select("title", "About | Ruby on Rails Tutorial Sample App")
+    self.assert_select("title", "About | #{@base_title}")
   end
   test "should get contact" do
-    self.get(static_pages_contact_url)
+    url = static_pages_contact_url
+    print "contact url = " + url +  "\n"
+    self.get(url)    
+#    self.get(static_pages_contact_url)
     assert_response :success
-    self.assert_select("title", "Contact | Ruby on Rails Tutorial Sample App")
+    self.assert_select("title", "Contact | #{@base_title}")
   end
   
   
