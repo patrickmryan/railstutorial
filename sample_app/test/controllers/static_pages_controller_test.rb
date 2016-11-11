3f7a4f91bbe0864a2525b0f185acc1d04017ad42
Patrick M. Ryan <pmryan@us.ibm.com>
2016-11-11 09:59:44 -0500

    some refactoring

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    #self.get(static_pages_home_url)
+  self.test "should get home" do
     self.get(:home)
     self.assert_response(:success)
     self.assert_select("title", "Ruby on Rails Tutorial Sample App")
   end
-
-  test "should get help" do
+  
+  
+  self.test "should get help" do
     self.get(:help)
     #get :help
     assert_response :success
     self.assert_select("title", "Help | Ruby on Rails Tutorial Sample App")
   end
 
-  test "should get about" do
+  self.test "should get about" do
     self.get(:about)
     #get :about
     assert_response :success
