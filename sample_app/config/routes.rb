0180246cd4435444da426d3de7279a10e9ba59ec
Patrick M. Ryan <pmryan@us.ibm.com>
2016-11-11 16:44:28 -0500

    default files for User login

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   self.root('static_pages#home')
   self.get('/help',    to: 'static_pages#help')
   self.get('/about',   to: 'static_pages#about')
