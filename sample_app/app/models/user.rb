5f9052c13c5220401da7d01ffda8be4a1850cf10
Patrick M. Ryan <pmryan@us.ibm.com>
2016-11-21 17:49:15 -0500

    working gemfile.  gets past section 6.3.4.

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ApplicationRecord
+
+  self.before_save { self.email = email.downcase }
+
   #  validates :name,  presence: true, length: { maximum: 50 }
   self.validates(:name, :presence => true, :length => { :maximum => 50 } )
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, 
-    presence: true, 
+  self.validates :email,
+    presence: true,
     length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
 
+  self.has_secure_password
+  self.validates :password, presence: true, length: { minimum: 6 }
 
 end
