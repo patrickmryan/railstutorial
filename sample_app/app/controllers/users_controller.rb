b0cdeb38b7fc2a324f75d38621d564bd21564472
Patrick M. Ryan <pmryan@us.ibm.com>
2016-11-22 17:40:35 -0500

    up to listing 7.19 user_params

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    #debugger
   end
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(self.user_params())
+    if @user.save()
+      # success
+    else
+      # unsuccessful
+      self.render('new')
+    end
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
+  end
+
 end
