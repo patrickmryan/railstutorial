55ca56bcb8aa56d06bf0f639dcd817d48fd05f6d
Patrick M. Ryan <pmryan@us.ibm.com>
2017-02-21 18:14:45 -0500

    fixed irritating deprecation warning

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -4,7 +4,6 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   fixtures :all
-
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
@@ -21,18 +20,21 @@ class ActionDispatch::IntegrationTest
   def log_in_as(user, options = {})
     password    = options[:password] || 'password'
     remember_me = options[:remember_me] || '1'
-     #debugger
+
     if (integration_test?)
-      self.post(self.login_path(), session: { email: user.email,
+      path = self.login_path()
+      self.post(path, params: { session: { email: user.email,
         password: password,
-        remember_me: remember_me })
+        remember_me: remember_me }})
+
     else
       self.session()[:user_id] = user.id
     end
 
   end
-  
+
   private
+
   def integration_test?
     # return true inside an integration test
     defined?(post_via_redirect)
