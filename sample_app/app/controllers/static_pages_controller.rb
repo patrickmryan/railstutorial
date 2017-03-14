2382aeb563c102676c6dd05a9f395d264e601abf
Patrick M. Ryan <pmryan@us.ibm.com>
2017-03-14 14:27:56 -0400

    final updates before starting ch. 11 (online)

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -1,8 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    puts "inside StaticPagesController home"
+#    puts "params -> "
+#    self.params().each do | key, value |
+#      puts "#{key} ----- #{value}"
+#    end
   end
 
   def help
+    puts "inside StaticPagesController help"
   end
   
   def about
